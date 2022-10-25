02b7f5adfcf4a78859ef1cc16c4640f21d4f5056
Yoshiki Sakai <y.s.west1009@gmail.com>
2022-10-24 21:23:18 -0700

    add : routing

---
+++ src/router/Router.tsx
@@ -0,0 +1,28 @@
+import {memo, VFC} from "react"
+import { Switch, Route } from "react-router-dom"
+
+import {Login } from "../components/pages/Login"
+import { Page404 } from "../components/pages/Page404"
+import { homeRoutes } from "./HomeRoutes"
+
+export const Router: VFC = memo(() =>{
+  return(
+    <Switch>
+      <Route exact path="/">
+        <Login/>
+      </Route>
+      <Route path="/home" render={({match:{url}}) =>(
+        <Switch>
+          {homeRoutes.map((route) =>(
+            <Route key={route.path} exact={route.exact} path={`${url}${route.path}`}>
+              {route.children}
+            </Route>
+          ))}
+        </Switch>
+      )} />
+      <Route path="*">
+        <Page404 />
+      </Route>
+    </Switch>
+  )
+})
